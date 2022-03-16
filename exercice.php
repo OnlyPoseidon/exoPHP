c7934192b9c451646256e0920236d11d43824c8a
OnlyPoseidon <MaSaMuNe43>
2022-03-16 16:21:04 +0100

    exo 8 suite

--- exercice.php
+++ exercice.php
@@ -465,7 +465,18 @@ while($pouet < 20) {
         if (strlen($argument) < 8 && )
 
     ------------------
-
-    
 */
+    function capital(string $argument) {
+        switch ($argument) {
+            case 0:
+                echo "i égal 0";
+                break;
+            case 1:
+                echo "i égal 1";
+                break;
+            case 2:
+                echo "i égal 2";
+                break;
+        }
+    }
 ?>
